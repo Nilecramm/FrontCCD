03d15f056ac22c9a975529853583e07f6863c08e
Nilecram <marcelin.ray@protonmail.com>
2026-02-19 22:26:58 +0100

    fix du bug de routage

--- front/frontend/src/App.jsx
+++ front/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import Register from './pages/subscriber/Register';
 import AdminDashboard from './pages/admin/Dashboard';
